91bbd54afdcddbbd53f1c961caa1f6038e43a345
jsk389 <jsk389@bham.ac.uk>
2016-11-02 17:09:34 +0000

    Attempt to combine three stations

---
+++ Code/combine_three_stations.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+from __future__ import division
+
+from copy import deepcopy
+
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+import pyfits
+import jdcal
+import FFT
+
+import pandas as pd
+
+from operator import itemgetter
+from itertools import groupby
+
+if __name__=="__main__":
+
+    main()
